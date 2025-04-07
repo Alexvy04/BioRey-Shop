9b9a06cd3e52355d1e6a6d4191686890b2783434
Alexvy04 <marijosita36@gmail.com>
2025-04-07 16:25:46 -0600

    Add files via upload

---
+++ biorey-store/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import BioReyStore from './components/BioReyStore'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BioReyStore />
+  </React.StrictMode>,
+)
